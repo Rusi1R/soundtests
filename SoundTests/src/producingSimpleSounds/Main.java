0e701e5ef5d4e19b9f0454a3a3387b92c284cb8a
ruslan <rsandlerr@gmail.com>
2017-07-26 20:35:37 +0200

    test

--- SoundTests/src/producingSimpleSounds/Main.java
+++ SoundTests/src/producingSimpleSounds/Main.java
@@ -18,31 +18,36 @@ public class Main {
 	 * @param sample
 	 * @throws LineUnavailableException
 	 */
-	private static void playSample(int[] sample) throws LineUnavailableException {
+	private static void playSample(int[][] samples) throws LineUnavailableException {
 		byte[] buf = new byte[2];
 		AudioFormat af = new AudioFormat((float) SAMPLERATE, SAMPLESIZE, 1, true, true);
 		SourceDataLine sdl = AudioSystem.getSourceDataLine(af);
 		sdl.open();
 		sdl.start();
-		for (int i = 0; i < (float) SAMPLERATE/sample.length; i++) {
-			for (int j = 0; j < sample.length; j++) {
-				double angle = i / ((float) sample[j]) * 2 * Math.PI;
-				buf[0] = (byte) (Math.sin(angle) * 100);
-				sdl.write(buf, 0, 2);
+
+		for (int[] sample : samples) {
+			for (int i = 0; i < (float) SAMPLERATE / sample.length; i++) {
+				for (int j = 0; j < sample.length; j++) {
+					double angle = i / ((float) sample[j]) * 2 * Math.PI;
+					buf[0] = (byte) (Math.sin(angle) * 100);
+					sdl.write(buf, 0, 2);
+				}
 			}
 		}
+
 		sdl.drain();
 		sdl.stop();
 	}
 
 	public static void main(String[] args) throws LineUnavailableException {
-		int[] sample = new int[]{ 180, 150 };
+		int[] sample = new int[] { 180, 150 };
+		int[] sample2 = new int[] { 190, 150 };
+		int[] sample3 = new int[] { 200, 150 };
 
-		for (int i = 0; i < 3; i++) {
-			playSample(sample);
-			sample[0] += 10;
-		}
-	
+		int[][] samples = new int[][] { sample, sample2, sample3 };
+
+		playSample(samples);
+
 	}
 
 }
