6248d608a34c59b500952f58061e9aabcd93eb99
Ruslan Sandler <rsandlerr@gmail.com>
2017-07-28 22:59:51 +0200

    refactoring

--- SoundTests/src/producingSimpleSounds/Main.java
+++ SoundTests/src/producingSimpleSounds/Main.java
@@ -1,52 +1,29 @@
 package producingSimpleSounds;
 
+import java.util.ArrayList;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
+
 public class Main {
-	// Wieviele Samples werden pro Sekunde abgespielt?
-	// 44.1kHz(Musik CD) - 48kHz(Film) - 96kHz(Tonstudio)
-	final static float SAMPLERATE = 1000 * 96;
-	// Anzahl der Bits pro Sample
-	final static int SAMPLESIZE = 16;
-
-	/**
-	 * plays a sample (one or multiple pitches at once)
-	 * 
-	 * @param sample
-	 * @throws LineUnavailableException
-	 */
-	private static void playSample(int[][] samples) throws LineUnavailableException {
-		byte[] buf = new byte[2];
-		AudioFormat af = new AudioFormat((float) SAMPLERATE, SAMPLESIZE, 1, true, true);
-		SourceDataLine sdl = AudioSystem.getSourceDataLine(af);
-		sdl.open();
-		sdl.start();
-
-		for (int[] sample : samples) {
-			for (int i = 0; i < (float) SAMPLERATE / sample.length; i++) {
-				for (int j = 0; j < sample.length; j++) {
-					double angle = i / ((float) sample[j]) * 2 * Math.PI;
-					buf[0] = (byte) (Math.sin(angle) * 100);
-					sdl.write(buf, 0, 2);
-				}
-			}
-		}
-
-		sdl.drain();
-		sdl.stop();
-	}
+	
 
 	public static void main(String[] args) throws LineUnavailableException {
-		int[] sample = new int[] { 180, 150 };
-		int[] sample2 = new int[] { 190, 150 };
-		int[] sample3 = new int[] { 200, 150 };
+		ArrayList<Sound> sounds = new ArrayList<Sound>();
+		
+		int[] sample = new int[] { Sound.B };
+		int[] sample2 = new int[] { Sound.Eh };
+		int[] sample3 = new int[] { Sound.G };
 
-		int[][] samples = new int[][] { sample, sample2, sample3 };
-
-		playSample(samples);
+		sounds.add(new Sound(sample, 40000));
+		sounds.add(new Sound(sample2, 45000));
+		sounds.add(new Sound(sample3, 40000));
+		
+		Sound.playSounds(sounds);
+		
+		
 
 	}
 
---
+++ SoundTests/src/producingSimpleSounds/Sound.java
@@ -0,0 +1,70 @@
+package producingSimpleSounds;
+
+import java.util.ArrayList;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+
+public class Sound {
+	// Wieviele Samples werden pro Sekunde abgespielt?
+	// 44.1kHz(Musik CD) - 48kHz(Film) - 96kHz(Tonstudio)
+	final static float SAMPLERATE = 1000 * 96;
+	// Anzahl der Bits pro Sample
+	final static int SAMPLESIZE = 8;
+
+	final static int DEFAULTSOUNDLENGTH = 35000;
+	
+	final static int Eh = 146;
+	final static int B = 195;
+	final static int G = 245;
+	final static int D = 328;
+	final static int A = 437;
+	final static int El = 560;
+	
+	
+
+	int[] samples;
+	int soundLength;
+
+	/**
+	 * 
+	 * @param samples Sound which is played. A sound consists of one or multiple samples
+	 * @param length How long the sound is played for (higher number = longer)
+	 */
+	public Sound(int[] samples, int length) {
+		this.samples = samples;
+		this.soundLength = length;
+	}
+
+	/**
+	 * plays a sample (one or multiple pitches at once)
+	 * 
+	 * @param sample
+	 * @throws LineUnavailableException
+	 */
+	static void playSounds(ArrayList<Sound> sounds) throws LineUnavailableException {
+		byte[] buf = new byte[5];
+		AudioFormat af = new AudioFormat((float) SAMPLERATE, SAMPLESIZE, 1, true, true);
+		SourceDataLine sdl = AudioSystem.getSourceDataLine(af);
+		sdl.open();
+		sdl.start();
+
+		for (Sound currentSound : sounds) {
+			for (int i = 0; i < (float) currentSound.soundLength; i++) {
+				for (int j = 0; j < currentSound.samples.length; j++) {
+					double angle = i/((float) currentSound.samples[j]) * 2 * Math.PI;
+					buf[0] = (byte) (Math.sin(angle) * 100);
+					sdl.write(buf, 0, 2);
+					System.out.println(buf[0]);
+				}
+			}
+		}
+
+		sdl.drain();
+		sdl.stop();
+	}
+	
+	
+
+}
